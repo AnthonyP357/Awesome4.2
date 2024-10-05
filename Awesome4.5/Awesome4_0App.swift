ad0bf3fe8ac88584d09a66e3961645d0cd107a53
Anthony Perez <aperez327@gmail.com>
2024-10-05 19:45:11 -0400

    Initial Commit

---
+++ Awesome4.5/Awesome4_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome4_0App.swift
+//  Awesome4.0
+//
+//  Created by Anthony Perez on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Awesome4_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome4.5/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Awesome4.0
+//
+//  Created by Anthony Perez on 9/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageString = "They Call You When Help Is Needed For Computing Solutions"
+    
+    var body: some View {
+        
+            VStack {
+                Text("You Are Learning SwiftUI!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                Image(systemName: "cloud.sun.rain.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                    .padding()
+                    .background(Color(hue: 0.493, saturation: 0.256, brightness: 0.958))
+                    .cornerRadius(30)
+                    .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                    .overlay(RoundedRectangle(cornerRadius: 30)
+                            .stroke(.teal, lineWidth: 1)
+                    )
+                    .padding()
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                   
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome!") {
+                        // This is the action performed when the button is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great!") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .padding()
+                
+                
+            }
+        }
+    }
+
+
+
+#Preview {
+    ContentView()
+}
